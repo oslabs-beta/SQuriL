2d7849231a05b75b5ddaf6d02892ee3ab6b565c3
Michael <mtrapani27@gmail.com>
2022-03-07 17:25:27 -0500

    added dashboard component and component boilerplate code

---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import QueryContainer from './QueryContainer';
+import SchemaContainer from './SchemaContainer';
+import URIInput from './URIInput';
+import Landing from './Landing';
+import OutputContainer from './OutputContainer';
+import Logo from './Logo';
+
+export default Dashboard;
