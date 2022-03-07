962146530cdb1ebb13066f09072956527512833b
Frank Nguyen <frankknguyen@gmail.com>
2022-03-07 15:57:21 -0600

    Installed pg module, Set up express boilerplate

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const { Pool } = require('pg');
 const app = express();
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const queryRouter = require(path.join(__dirname, '/routers/queryRouter.js'));
+const apiRouter = require(path.join(__dirname, '/routers/apiRouter.js'));
+const userRouter = require(path.join(__dirname, '/routers/userRouter.js'));
 
+require('dotenv').config();
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URI
+});
 
+// Route requests to queryRouter
+app.use('query', queryRouter);
+
+// Route requests to apiRouter
+app.use('api', apiRouter);
+
+// Route requests to outputRouter
+app.use('user', userRouter);
 
 // Unknown Route Handler
 app.use((req, res) => res.sendStatus(404));
