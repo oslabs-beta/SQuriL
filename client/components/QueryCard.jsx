a3a7773f80f4a04c07b5c3972ad9e8fa12e1b3dd
Michael <mtrapani27@gmail.com>
2022-03-09 12:26:56 -0500

    continued schema and output container work, and added header component

--- client/components/QueryCard.jsx
+++ client/components/QueryCard.jsx
@@ -6,10 +6,10 @@ import styles from '../Styles/QueryContainer.css'
 function QueryCard(props) {
     return (
         <div className='QueryCard'>
-            <h3 style={{ cursor: 'pointer ' }}>
+            <h4 style={{ cursor: 'pointer ' }}>
                 {props.queryCard}
             <Button variant='text' size='small' color='inherit' className='deleteQuery'>X</Button>
-            </h3>
+            </h4>
         </div>
     );
 }
