a10fe4dd193a880006b6842747543e6e228b0436
ruslanovna <ruslanovna.kim@gmail.com>
2022-03-09 11:18:34 -0500

    add create query connected to sql database

---
+++ server/db/db.js
@@ -0,0 +1,21 @@
+// THIS DOESN'T WORK -> IT BEING IN THE DB.JS FILE 
+// WE MOVED THE POOL INSTANCE TO THE QUERY_CONTROLLER
+
+
+// POOL - allows multiple simultaneous database queries
+const { Pool } = require('pg');
+
+// create a new pool here using the connection string above
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URI,
+});
+
+// We export an object that contains a property called query,
+// which is a function that returns the invocation of pool.query() after logging the query
+// This will be required in the controllers to be the access point to the database
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text.params, callback)
+  }
+}
