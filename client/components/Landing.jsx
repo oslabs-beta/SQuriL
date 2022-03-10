8e4b0498095be76ac16e819539ff973c31c340ba
Michael <mtrapani27@gmail.com>
2022-03-09 20:02:22 -0500

    additional header updates

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import LogoLanding from './LogoLanding';
 import OAuth from './OAuth';
 import styles from '../Styles/Landing.css'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 // create a component that serves as login button and fx
-function Landing (props) {
-    return(
+function Landing(props) {
+    return (
         <div className="Landing">
-            <div className='Landing-inner'>
-        <LogoLanding />    
-        <OAuth />
-        </div>
-        </div>
+            <LogoLanding />
+            {/* <div className='Tagline'>
+        <h3>Stach your Cache</h3>
+        </div>   
+        <OAuth /> */}
+ <SyntaxHighlighter language="javascript" style={docco}>
+     {'hello World'}
+    </SyntaxHighlighter>
+        </div > 
     )
 }
 
--- client/components/OAuth.jsx
+++ client/components/OAuth.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import styles from '../Styles/Landing.css'
 
 function OAuth() {
     return (
         <Button
+            className='OAuth'
             variant='outlined'
             startIcon={<GitHubIcon />}
         //onClick method will go here
