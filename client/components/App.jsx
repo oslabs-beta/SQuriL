7c8b7b2a53678453b7775da3a0c764ae324b59fa
Michael <mtrapani27@gmail.com>
2022-03-13 19:10:12 -0400

    changed light dark mode icon

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 // import Dashboard from './Dashboard.jsx'
 import { Button, CssBaseline, FormControlLabel, FormGroup, Switch } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton'
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 import ModalDialogue from './ModalDialogue'
 import black_logo from '../Public/black_logo.png'
 import logo from '../Public/logo.png'
@@ -43,10 +46,12 @@ function App() {
         <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
             <CssBaseline />
         <div className='App'>
-            <FormGroup>
-                <FormControlLabel control={ <Switch checked={isDarkTheme} onChange={changeTheme}/>}
-                label="Dark Theme" />
-            </FormGroup>
+            <span>
+            { isDarkTheme ? 'dark mode' : 'light mode'}
+            <IconButton sx={{ ml: 1 }} onClick={changeTheme} color='inherit'>
+                { isDarkTheme ? <Brightness7Icon /> : <Brightness4Icon /> }
+            </IconButton>
+            </span>
             <div className='app-welcome' style={isDarkTheme ? {border: '2px solid white'} : {border: '2px solid black'}}>
             <img src={isDarkTheme ? logo : black_logo} alt='logo' className='logo' />
             <Button variant='contained' color='primary' onClick={handleOpen}>
