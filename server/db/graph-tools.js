cd3ecc732aca82514684854b9b271cc90fba8ba5
ruslanovna <ruslanovna.kim@gmail.com>
2022-03-16 23:54:45 -0400

    working gql template for MVP

--- server/db/graph-tools.js
+++ server/db/graph-tools.js
@@ -3,41 +3,35 @@
 // queries: 1 user 
 
 
-const typeDefs = `
-  // describe tables 
+const typeDefs = gql`
+  # describe tables field
+  # ! means its unique
   type Users {
-    _id: ID!
-    username: String!
+    _id: ID
+    username: String
   }
-
   type Queries {
     _id: ID!
     value: String
-    user_id: Users! //1 required user / query ID
+    user_id: Users #1 required user / query ID ****
   }
 
+  # define inputs to be used in Query resolvers functions
   input Queries_by_Users {
     user_id: ID!
   }
-
-
-  // this schema allows the following query:
+  # this schema allows the following query:
+  # ! means its required
   type Query {
-    // info on one user
-    perUsers(id: ID!): Users  
-    // info on all users 
+    # info on one user
+    Users(_id: ID!): [Users]  #so even though we get one user, we put [Users] for graphQl map out the results per parameters in the output
+    # info on all users 
     allUsers: [Users]
 
-    // possible queries for Query table
-    perQueries(id: ID!): Queries
+    # possible queries for Query table
+    Queries(_id: ID!): [Queries]
     allQueries: [Queries]
-    Queries_by_Users(where: Queries_by_Users ): [Queries] 
-  }
-
-  # we need to tell the server which types represent the root query
-  # and root mutation types. We call them RootQuery and RootMutation by convention.
-  schema {
-    query: Query
+    Queries_by_Users(find: Queries_by_Users ): [Queries] 
   }
 `
 const resolvers = {
@@ -45,34 +39,54 @@ const resolvers = {
   Query: {
     // show the rest of the info on the user_id = $
     Users: async (parent, args, context, info) => {
-      const data = await db.query(`SELECT * FROM users WHERE _id='${args}'`)
-      const result = data.rows.json()
-      return result;
+      try {
+        const data = await db.query(`SELECT * FROM users WHERE _id='${args._id}'`)
+        return data.rows;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
     // show all users that exist in the tb
     allUsers: async (parent, args, context, info) => {
-      const data = await db.query('SELECT * FROM users')
-      const result = data.rows.json()
-      return result;
+      try {
+        const data = await db.query('SELECT * FROM users')
+        return data.rows; 
+      } catch (error) {
+        throw new Error(error)
+      }
     },
     // show query based on this id
     Queries: async (parent, args, context, info) => {
-      const data = await db.query(`SELECT * FROM queries WHERE _id='${args}'`)
-      const result = data.rows.json()
-      return result;
+      try {
+        const data = await db.query(`SELECT * FROM queries WHERE _id='${args._id}'`)
+        return data.rows;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
     // show all existing queries
     allQueries: async (parent, args, context, info) => {
-      const data = await db.query('SELECT * FROM queries')
-      const result = data.rows.json()
-      return result;
+      console.log(args)
+      try {
+        const data = await db.query('SELECT * FROM queries')
+        console.log(data.rows)
+        return data.rows;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
+    // many to one
     // show me all queries based on user_id (filter by users aka args act like filters to show only selected)
     Queries_by_Users: async (parent, args, context, info) => {
       const data = await db.query('SELECT _id, value FROM queries q LEFT OUTER JOIN users u on q.user_id=u._id WHERE u.username=$1')
       const result = data.rows.json 
-    }      
+    },      
   }
 }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 
