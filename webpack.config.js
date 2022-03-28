70af87160155d51923218c15eb64acb53668bc01
Michael <mtrapani27@gmail.com>
2022-03-28 11:45:23 -0400

    file changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-1//direct the files
-const path = require('path');
-// plugin automatically add css, js ,... assets from your entry point to your html output. 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// direct the files
 
+const path = require('path');
+// plugin automatically add css, js ,... assets from your entry point to your html output.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
   // set the entry and output points for the webpack to initiate compiling
   entry: {
-    src: './client/main.tsx'
+    src: './client/main.tsx',
   },
   output: {
     // in production, the bundle will live on file System
@@ -24,15 +24,13 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/env', '@babel/react']
-        }
+          presets: ['@babel/env', '@babel/react'],
+        },
       },
       {
         test: /\.s?css/,
         // exclude: /node_module/,
-        use: [
-          'style-loader', 'css-loader', 'sass-loader',
-        ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       // Font and SVGs
       {
@@ -44,8 +42,8 @@ module.exports = {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
       },
-       // added ts-loader to load our typescript files
-       {
+      // added ts-loader to load our typescript files
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
@@ -63,20 +61,20 @@ module.exports = {
     // its where the bundle.js will live on RAM during development?
     static: {
       publicPath: '/build',
-      directory: path.resolve(__dirname, 'build')
-    }, 
+      directory: path.resolve(__dirname, 'build'),
+    },
     // set up the proxy such that you can call API requests from hot-reload webpack server to the express back-end server
     // aka fetch req. from localhoast:8080/api/* redirect to localhost:3000/api/*
     // proxy: {
     //   '/convert/**': 'http://localhost:3000',
     //   secure: false,
-    // } 
+    // }
     proxy: {
       // for API request in schemas generations in URIinput component
       '/api/*': {
         target: 'http://localhost:3000/',
         secure: false,
-      }, 
+      },
       // for managing the saved queries
       '/query/*': {
         target: 'http://localhost:3000/',
@@ -97,12 +95,11 @@ module.exports = {
         target: 'http://localhost:3000/',
         secure: false,
       },
-    }
+    },
   },
   // Enable importing JS / JSX files without specifying their extension
   // Adding TS and TSX files here
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-
 };
