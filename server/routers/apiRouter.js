94580f89e26852d79549cc6970bd04b4489e812c
Frank Nguyen <frankknguyen@gmail.com>
2022-03-30 09:24:07 -0500

    Functionality of rewriting the file should work after integrating

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -9,9 +9,9 @@ const resolverController = require('../controllers/resolverController');
 router.post(
   '/createGqlSchema',
   // grab all table values and structures
-  // apiController.emptyInput,
+  apiController.emptyInput, // clears the input file for codegen
   apiController.getDBName, // res.locals.DBname
-  apiController.getTableInfo, // res.locals.SQLSchema
+  apiController.getTableInfo, // res.locals.SQLSchema'
   typeDefController.generateTypeDef, // res.locals.typeDefs
   resolverController.generateResolvers, // res.locals.resolvers
   (req, res) => {
@@ -22,9 +22,5 @@ router.post(
 router.get('/codegen', apiController.codegen, apiController.readFile, (req, res) => {
   res.status(200).json(res.locals.output);
 });
-
-// router.get('/reset', apiController.emptyInput, (req, res) => {
-//   res.status(200).send('i did it');
-// });
 
 module.exports = router;
