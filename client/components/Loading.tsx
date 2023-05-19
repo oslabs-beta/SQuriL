5a81b0d480e2da06d690c2f4c56c36f7926ed9a8
Mark Charles Smith <markcharlessmith@gmail.com>
2023-05-18 18:27:58 -0700

    cleaned up code in Loading.tsx

--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import ReactLoading from 'react-loading';
-// import PropTypes from 'prop-types';
-// import '../Styles/SchemaContainer.css';
-
-// function Loading(props) {
-//   const { isDarkTheme } = props;
-//   return (
-//     <div className='loading'>
-//       <ReactLoading type='bars' color={isDarkTheme ? 'rgb(72, 20, 155)' : '#aedb95'} />
-//     </div>
-//   );
-// }
-
-// Loading.propTypes = {
-//   isDarkTheme: PropTypes.bool,
-// };
-
-// export default Loading;
-
 import React from 'react';
 import ReactLoading from 'react-loading';
 import '../Styles/SchemaContainer.css';
